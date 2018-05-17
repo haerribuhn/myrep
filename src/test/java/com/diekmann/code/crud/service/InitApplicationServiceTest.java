87757696ef658ca1585c11c25ae18be29eb391a6
haerribuhn <rainer.diekmann@hamburg.de>
2018-05-17 20:31:04 +0200

    Add tests

---
+++ src/test/java/com/diekmann/code/crud/service/InitApplicationServiceTest.java
@@ -0,0 +1,36 @@
+package com.diekmann.code.crud.service;
+
+import com.diekmann.code.crud.domain.Notes;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class InitApplicationServiceTest {
+
+    @Mock
+    private NotesServiceImpl notesService;
+
+    @InjectMocks
+    private InitApplicationService initApplicationService;
+
+    @Test
+    public void initializeTestData() {
+        //given
+        given(notesService.saveNotes(any())).willReturn(any());
+
+        //when
+        initApplicationService.initializeTestData();
+
+        //then
+        verify(notesService, times(2)).saveNotes(any());
+
+    }
+}
