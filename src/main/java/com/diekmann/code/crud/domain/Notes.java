75b409d3ca7d70642d3e2ed3d5f411a89e4d4e6e
haerribuhn <Rainer.Diekmann@hamburg.de>
2023-03-29 09:28:07 +0000

    Sprintboot version 3.0.5

--- src/main/java/com/diekmann/code/crud/domain/Notes.java
+++ src/main/java/com/diekmann/code/crud/domain/Notes.java
@@ -1,7 +1,10 @@
 package com.diekmann.code.crud.domain;
 
-
-import javax.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 /**
  * Created by abraun on 23/11/2017.
